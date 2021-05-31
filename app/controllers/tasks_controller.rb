d5448139cda7da78c3d9462cb288e95f8f4f9c40
ten <kamizyou05@yahoo.co.jp>
2021-05-31 08:05:59 +0000

    DRY and validation

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -25,11 +26,9 @@ class TasksController < ApplicationController
 
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -41,7 +40,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -52,7 +51,10 @@ class TasksController < ApplicationController
 
   private
 
-  # Strong Parameter
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
   def task_params
     params.require(:task).permit(:content)
   end
