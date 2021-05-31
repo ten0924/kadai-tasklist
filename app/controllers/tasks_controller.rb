e28d0a0f5a7eb93b8a2af4822df6405138b28600
ten <kamizyou05@yahoo.co.jp>
2021-05-31 08:20:07 +0000

    add column

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -56,7 +56,7 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:status,:content)
   end
   
 end
