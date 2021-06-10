14e00dbafebc903817e30ac7fa357ae12f687e5a
ten <kamizyou05@yahoo.co.jp>
2021-06-10 08:59:00 +0000

    add modelUser columnUserreferences

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   root to: 'tasks#index'
+  
+  # get 'login', to: 'tasks#new'
+  # post 'login', to: 'tasks#create'
+  # delete 'logout', to: 'tasks#destroy'
+  
   resources :tasks
 end
+
+
+# Rails.application.routes.draw do
+#   root to: 'toppages#index'
+
+
+#   get 'signup', to: 'users#new'
+#   resources :users, only: [:index, :show, :create]
+  
+#   resources :microposts, only: [:create, :destroy]
+# end
